ed58e401b8d402cc404ff6931ec0790bf0bbc744
Kaio Ferreira <121730814+KaioFerr@users.noreply.github.com>
2024-08-12 18:12:43 -0300

    Update vetor_matriz.cpp

--- vetor_matriz.cpp
+++ vetor_matriz.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <math.h>
-#include<algorithm>
+#include <cmath>
+#include <algorithm>
+#include <stdbool.h>
 
 using namespace std;
 
 void exercicio01(){
+    cout << endl << "=== EXERCICIO 01 =====" << endl;
     int vetor[5];
     int soma = 0;
     for (int i = 0; i < 5; i++){
@@ -14,7 +16,7 @@ void exercicio01(){
             soma += vetor[i];
         }
     }
-    cout << soma;
+    cout << "Soma: " << soma << endl;
 }
 
 void exercicio02(){
@@ -38,7 +40,7 @@ void exercicio02(){
         precoTotal += precoDeCadaItem;
     }
 
-    cout << "Preco Total: " << precoTotal;
+    cout << "Preco Total: " << precoTotal << endl;
 }
 
 
@@ -51,41 +53,41 @@ void exercicio03(){
         cin >> vetorNotas[i-1];
         media += vetorNotas[i-1] / 5;
     }
-    
+
     for (int i = 1; i < 6; i++){
         desvio = (vetorNotas[i-1] - media);
         vetorDesvios[i-1] = desvio;
-        cout << "\nDesvio " << i << ": " << vetorDesvios[i-1]; 
+        cout << "\nDesvio " << i << ": " << vetorDesvios[i-1];
         somatorio += pow((desvio), 2);
     }
 
     desvioPadrao = sqrt((somatorio/5));
-    cout << "Desvio Padrão: " << desvioPadrao;
+    cout << "Desvio Padrão: " << desvioPadrao << endl;
 }
 
 void exercicio04(){
     int tamanhoDaLista;
-    
+
     cout << "Digite o tamanho da sua lista (máximo 10): ";
     cin >> tamanhoDaLista;
-    
+
     int lista[tamanhoDaLista];
-    
+
     for (int i = 0; i < tamanhoDaLista; i++){
         cout << "Digite o elemento " << i+1 << ": ";
         cin >> lista[i];
     }
-    
+
     sort(lista, lista + tamanhoDaLista);
     cout << endl;
-    
+
     for (int i = 0; i < tamanhoDaLista; i++){
         cout << "Elementos " << i+1 << ": " << lista[i] << endl;
         fflush(stdin);
-        
+
     }
-    
-    cout << "Os dois maiores Elementos: " << lista[tamanhoDaLista-2] << ", " << lista[tamanhoDaLista - 1];   
+
+    cout << "Os dois maiores Elementos: " << lista[tamanhoDaLista-2] << ", " << lista[tamanhoDaLista - 1] << endl;
 }
 
 void exercicio05() {
@@ -110,39 +112,93 @@ void exercicio05() {
     cout << "Segundo Vetor: " << endl;
     for (int i = 0; i < tamanhoDoVetor; i++)
     {
-         cout << "elemento da posicao " << i << ": " << segundoVetor[i] << endl;
-    }    
+        cout << "elemento da posicao " << i << ": " << segundoVetor[i] << endl;
+    }
 }
 
 void exercicio06() {
-    int m = 2, n = 3;
-    int primeiraMatriz[m][n];
-    int matrizTransposta[m][n];
+    int m, n;
 
-    for (int i = 0; i < 2; i++)
+    cout << "Digite a quantidade de linhas (M): ";
+    cin >> m;
+
+    cout << "Digite a quantidade de colunas (N): ";
+    cin >> n;
+
+    int primeiraMatriz[m][n];
+
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < n; j++)
         {
             cout << "Digite o valor da posicao " << i << " e " << j << ": ";
             cin >> primeiraMatriz[i][j];
         }
-        
     }
 
     cout << endl << "Primeira Matriz:" << endl;
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < n; j++)
         {
             cout << " " << primeiraMatriz[i][j];
         }
         cout << endl;
     }
-    
+
+    cout << endl << "Matriz Transposta:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << "  " << primeiraMatriz[j][i];
+        }
+        cout << endl;
+    }
+
 }
 
 int main() {
-    exercicio06();
+
+    bool verificador = true;
+
+    while (verificador) {
+
+        int opcao;
+        cout << endl << "===== EXERCICIOS DE VETORES ==" << endl << "Escolha uma das opcoes abaixo:" << endl;
+        for (int i = 1; i <= 6; i++){
+            cout << i << " - exercicio 0" << i << endl;
+        }
+
+        cout << "Digite uma opcao (digite 7 para sair): ";
+        cin >> opcao;
+
+        switch(opcao) {
+            case 1:
+                exercicio01();
+                break;
+            case 2:
+                exercicio02();
+                break;
+            case 3:
+                exercicio03();
+                break;
+            case 4:
+                exercicio04();
+                break;
+            case 5:
+                exercicio05();
+                break;
+            case 6:
+                exercicio06();
+                break;
+            case 7:
+                verificador = false;
+                break;
+            default:
+                cout << "Voce digitou a opcao errada" << endl;
+                break;
+        }
+    }
     return 0;
 }
-
